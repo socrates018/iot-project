15336ae993c27e7433ce5860e1bdfe05ac1ca4f1
socrates018 <socratessuciu8@gmail.com>
2025-05-11 21:07:37 +0300

    Add files via upload

--- esp32_blink_test/main_example.py
+++ esp32_blink_test/main_example.py
@@ -1,1 +1,13 @@
+import machine
+import time
 
+def main():
+    led = machine.Pin(8, machine.Pin.OUT)  # Use pin 8 for ESP32-C3 onboard LED
+    while True:
+        led.value(1)  # LED on
+        time.sleep(0.5)
+        led.value(0)  # LED off
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
